0b62cfb20a66b2b5e0ea94a43d9a25bc5572317b
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2024-06-14 19:28:32 +0100

    Update mutation-caller.py

--- mutation-caller.py
+++ mutation-caller.py
@@ -1,15 +1,16 @@
 ########### This software works with unzipped reference genome file
 
+
 import subprocess
 import pandas as  pd
 import os
 import glob
 import os.path
 from subprocess import call
-import rpy2
-import rpy2.robjects as robjects
-import matplotlib.pyplot as plt
-import altair as alt
+# import rpy2
+# import rpy2.robjects as robjects
+# import matplotlib.pyplot as plt
+# import altair as alt
 
 # Initial User interface
 
@@ -47,7 +48,7 @@ os.mkdir ('results')
 os.chdir('results')
 
 
-output_folder = ['sam', 'bam', 'bcf', 'vcf']
+output_folder = ['sam', 'bam', 'bcf', 'vcf', 'dndscv']
 for folder in output_folder:
     os.mkdir(folder)
 
@@ -61,6 +62,7 @@ os.chdir(wd)
 
 
 
+
 for fq1 in os.path.join (wd, '*_R1.fastq.gz'):
     
     file = glob.glob("*R1.fastq.gz")
@@ -68,42 +70,91 @@ for fq1 in os.path.join (wd, '*_R1.fastq.gz'):
         pattern = item.find("_")
    
         base = item[0:pattern]
-        base_R1 = base + '_R1.fastq.gz'
-        base_R2 = base + '_R2.fastq.gz'
-        base_sam = base + '.aligned.sam'
-        base_bam = base + '.aligned.bam'
-        base_sorted_bam = base + '.aligned.sorted.bam'
-        base_bcf = base + '_raw.bcf'
-        base_variant = base + '_variants.vcf'
-        base_final_variant = base + '_final_variants.vcf'
-        base_indel = base + '_indels.vcf.gz'
-        base_snv = base + '_snvs.vcf.gz'
+        print (f'base name is {base}')
+        fq1= os.path.join (wd, f'{base}' + '_R1.fastq.gz')
+        fq2= os.path.join (wd, f'{base}' + '_R2.fastq.gz')
+        sam = os.path.join (wd, 'results', 'sam', f'{base}' + '.aligned.sam')
+        bam = os.path.join (wd, 'results', 'bam', f'{base}' + '.aligned.bam')
+        sorted_bam = os.path.join (wd, 'results', 'bam', f'{base}' + '.aligned.sorted.bam')
+        raw_bcf = os.path.join (wd, 'results', 'bcf', f'{base}' + '_raw.bcf')
+        variants = os.path.join (wd, 'results', 'vcf', f'{base}' + '_variants.vcf')
+        final_variants = os.path.join (wd, 'results', 'vcf', f'{base}' + '_final_variants.vcf')
+        indexed_variants = os.path.join (wd, 'results', 'vcf', f'{base}' + '_final_variants.vcf.gz')
+        indels = os.path.join (wd, 'results', 'vcf', 'indels', f'{base}' + '_indels.vcf.gz')
+        snvs = os.path.join (wd, 'results', 'vcf', 'snvs', f'{base}' + '_snvs.vcf.gz')
+        
 
-        fq1 = os.path.join (wd, base_R1)
-        fq2 = os.path.join (wd, base_R2)
-        sam = os.path.join (wd, 'results', 'sam', base_sam)
-        bam = os.path.join (wd,'results', 'bam', base_bam)
-        sorted_bam = os.path.join (wd,'results', 'bam', base_sorted_bam)
-        raw_bcf = os.path.join (wd,'results','bcf', base_bcf)
-        variants = os.path.join (wd, 'results', 'vcf', base_variant)
-        final_variants = os.path.join (wd, 'results', 'vcf', base_final_variant)
-        indexed_variants = os.path.join (wd, 'results', 'vcf', base_final_variant)
-        indels = os.path.join (wd, 'results', 'vcf', 'indels', base_indel)
-        snvs = os.path.join (wd, 'results', 'vcf', 'snvs', base_snv)
+
+        
 
 
 
+
+        
         for command in (f"bwa mem {ref_genome_file_path} {fq1} {fq2} > {sam}", 
                         f"samtools view -S -b {sam} > {bam}",
                         f"samtools sort -o {sorted_bam} {bam}",
                         f"samtools index {sorted_bam}", 
                         f"bcftools mpileup -O b -o {raw_bcf} -f {ref_genome_file_path} {sorted_bam}",
                         f"bcftools call --ploidy 1 -m -v -o {variants} {raw_bcf}",
+                        f"vcfutils.pl varFilter {variants} > {final_variants}",
                         f"bgzip {final_variants}",
                         f"bcftools index {indexed_variants}",
-                        f"bcftools view -v snps {indexed_variants} > {snvs}",
-                        f"bcftools view -v indels {indexed_variants} > {indels}"):
+                        f"bcftools view -v snps {indexed_variants} > -Oz -o {snvs}", # could we call variants from variants, not final_variants?
+                        f"bcftools view -v indels {indexed_variants} > -Oz -o {indels}"):
               
 
               
             call(command, shell=True) 
+        # Extracting indels variants
+
+        print ("Contructing indels mutation table...") 
+
+        
+        os.chdir(os.path.join (wd, 'results', 'vcf', 'indels'))
+
+        file = glob.glob("*vcf.gz")
+        all_samples=[]
+        for item in file:
+                pattern = item.find("_")
+
+                sampleID = item[0:pattern]
+                df_variant=pd.read_csv(item,sep='\t',comment='#',usecols=[0,1,3,4],names=['CHR','POS','REF','ALT'])
+                sampleids=[sampleID for i in range(df_variant.shape[0])]
+                df_variant.insert(0,'SampleID',sampleids)
+                all_samples.append(df_variant)
+            
+                print(f"Generating indels mutation table from {sampleID}")
+
+            
+       
+        df_final=pd.concat(all_samples)
+        df_final.to_csv('combined_indels_variants.csv',index=False)
+
+            
+        # Extracting snvs variants
+
+        print ("Contructing snvs mutation table...") 
+
+        #wd = os.getcwd()
+        os.chdir(os.path.join (wd, 'results', 'vcf', 'snvs'))
+
+        file = glob.glob("*vcf.gz")
+        all_samples=[]
+        for item in file:
+                pattern = item.find("_")
+
+                sampleID = item[0:pattern]
+                df_variant=pd.read_csv(item,sep='\t',comment='#',usecols=[0,1,3,4],names=['CHR','POS','REF','ALT'])
+                sampleids=[sampleID for i in range(df_variant.shape[0])]
+                df_variant.insert(0,'SampleID',sampleids)
+                all_samples.append(df_variant)
+            
+                print(f"Generating snvs mutation table from {sampleID}")
+
+            
+        df_final=pd.concat(all_samples)
+        df_final.to_csv('combined_snv_variants.csv',index=False, header=None)
+
+     
+    
