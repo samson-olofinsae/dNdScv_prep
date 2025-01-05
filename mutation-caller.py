fbcba5c02524c565e1dd930d6a112023e38dfdd1
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-01-05 22:05:31 +0000

    Update mutation-caller.py

--- mutation-caller.py
+++ mutation-caller.py
@@ -76,7 +76,6 @@ for fq1 in os.path.join (wd, '*_R1.fastq.gz'):
         snvs = os.path.join (wd, 'results', 'vcf', 'snvs', f'{base}' + '_snvs.vcf')
         comprssed_snvs = os.path.join (wd, 'results', 'vcf', 'snvs', f'{base}' + '_snvs.vcf.gz')
 
-
               
         for command in (f"bwa mem {ref_genome_file} {fq1} {fq2} | samtools sort -o {sorted_bam}",
                         f"samtools index -b {sorted_bam}", 
